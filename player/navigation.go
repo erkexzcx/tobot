ace74daeacdb86bbbaf782599e65b1d4b4a24a48
Erikas Rudinskas <erikmnkl@gmail.com>
2021-11-21 13:40:53 +0200

    remove debug line

--- player/navigation.go
+++ player/navigation.go
@@ -255,7 +255,6 @@ func (p *Player) updateBecomeOfflineTimes() {
 func (p *Player) randomWait() {
 	if p.randomizeWaitTo != 0 {
 		timeToWait := time.Duration(getRandomInt64(int64(p.randomizeWaitFrom), int64(p.randomizeWaitTo)))
-		log.Println("[" + p.nick + "] additionally sleeps " + timeToWait.String())
 		time.Sleep(timeToWait)
 	}
 }
