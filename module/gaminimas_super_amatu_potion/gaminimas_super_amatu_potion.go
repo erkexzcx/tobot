ad6e231c3f1b979f7848dad23fb81ecc35cddbdc
Erikas <erkexzcx@users.noreply.github.com>
2023-05-08 01:26:54 +0300

    try another fix

--- module/gaminimas_super_amatu_potion/gaminimas_super_amatu_potion.go
+++ module/gaminimas_super_amatu_potion/gaminimas_super_amatu_potion.go
@@ -67,7 +67,7 @@ func (obj *GaminimasSuperAmatuPotion) Perform(p *player.Player, settings map[str
 	}
 
 	// Check if not depleted
-	if doc.Find(":contains('Nepakanka reikiamų uogų!')").Length() > 0 {
+	if doc.Find("div:contains('Nepakanka reikiamų uogų!')").Length() > 0 {
 		return &module.Result{CanRepeat: false, Error: nil}
 	}
 
@@ -98,7 +98,7 @@ func (obj *GaminimasSuperAmatuPotion) Perform(p *player.Player, settings map[str
 	// Check if not depleted. If this is not checked, same error message
 	// would be shown in action page and result in error.
 	// TODO: Understand how to avoid this?
-	if doc.Find(":contains('Nepakanka reikiamų')").Length() > 0 {
+	if doc.Find("div:contains('Nepakanka reikiamų uogų!')").Length() > 0 {
 		return &module.Result{CanRepeat: false, Error: nil}
 	}
 
