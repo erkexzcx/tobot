73542a04dc31192700de58485e1f99b56dbbbc29
Erikas <erkexzcx@users.noreply.github.com>
2023-05-21 23:30:29 +0300

    don't drop all at once

--- player/drop_lem.go
+++ player/drop_lem.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/url"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -14,20 +15,27 @@ import (
 // Find out how much we have (POST request)
 // http://tob.lt/zaisti.php?{{ creds }}&id=drop2&ka=LEM
 
+var lemDropMux = sync.Mutex{}
+
 func (p *Player) dropAllLEM() error {
+	lemDropMux.Lock()
+
 	path := "/zaisti.php?{{ creds }}&id=drop&ka=LEM"
 	doc, wrongDoc, err := p.Navigate(path, false)
 	if err != nil {
+		lemDropMux.Unlock()
 		return err
 	}
 	if wrongDoc {
 		p.Log.Warning("Got wrong document during LEM drop, trying again")
+		lemDropMux.Unlock()
 		return p.dropAllLEM()
 	}
 
 	maxToDrop, found := doc.Find("form > input[name='kiekis'][type='hidden']").Attr("value")
 	if !found {
 		p.Log.Warning("No LEM drop, exiting")
+		lemDropMux.Unlock()
 		return nil // Probably empty page, nothing to drop
 	}
 
@@ -41,25 +49,30 @@ func (p *Player) dropAllLEM() error {
 	path = "/zaisti.php?{{ creds }}&id=drop2&ka=LEM"
 	doc, wrongDoc, err = p.Submit(path, body)
 	if err != nil {
+		lemDropMux.Unlock()
 		return err
 	}
 	if wrongDoc {
 		p.Log.Warning("Got wrong document during LEM drop submit, trying again")
+		lemDropMux.Unlock()
 		return p.dropAllLEM()
 	}
 
 	if doc.Find("div:contains('Daiktai išmesti')").Length() > 0 {
+		lemDropMux.Unlock()
 		return nil
 	}
 
 	if doc.Find("div:contains('Antiflood! Bandykite už kelių sekundžių.')").Length() > 0 {
 		p.Log.Warning("Got too fast click during LEM drop submit, trying again in 5 seconds")
 		time.Sleep(5 * time.Second)
+		lemDropMux.Unlock()
 		return p.dropAllLEM()
 	}
 
 	html, _ := doc.Html()
 	fmt.Println(html)
 
+	lemDropMux.Unlock()
 	return errors.New("unknown error occurred during dropping LEMs")
 }
